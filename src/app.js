4f7c91ab1d350382c4668c57f18694c705ba61e1
Mkparu <mkparusomtochi26@gmail.com>
2023-07-05 02:37:48 +0100

    chore: added comments in js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// all the html elements we need to manipulate.
 const gameSelectLogo = document.querySelector("#game-select-logo");
 const btnMark = document.querySelectorAll(".btn-mark");
 const btnSelect = document.querySelectorAll(".btn-select");
@@ -14,6 +15,8 @@ const tiesScore = document.querySelector("#ties-score");
 const p1Name = document.querySelector("#p1-name");
 const p2Name = document.querySelector("#p2-name");
 const btnResult = document.querySelectorAll(".btn-result");
+
+// data for game to run properly.
 // have an initial game state, load from local storage if it exists
 let gameState = {
   playerTurn: "X",
@@ -43,7 +46,8 @@ const resultStyles = {
   },
   O: {
     class: "result-O",
-    blueSvg:'<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="80px" viewBox="0 0 64 64"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#1F3641"/></svg>',
+    blueSvg:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="80px" viewBox="0 0 64 64"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#1F3641"/></svg>',
     svg: '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="80px" viewBox="0 0 64 64"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>',
   },
 };
@@ -57,7 +61,7 @@ const winningCombinations = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-// game select functions
+// allows the user to toggle between X and O
 function handleMarkSelect(index) {
   btnMark[(index + 1) % btnMark.length].className = "btn-mark";
   btnMark[index].className += " selected";
@@ -66,7 +70,7 @@ function handleMarkSelect(index) {
 btnMark.forEach((btn, index) => {
   btn.addEventListener("click", () => handleMarkSelect(index));
 });
-
+// handles all the choices gotten from game select modal
 function handleGameOptionChoice(index) {
   gameState.cpu = btnSelect[index].dataset.select === "cpu";
   gameState.player2 = btnSelect[index].dataset.select === "player2";
@@ -77,37 +81,35 @@ function handleGameOptionChoice(index) {
   gameSelectLogo.style.display = "none";
   gameSelect.style.display = "none";
   gameScreen.style.display = "flex";
+  // we create the board after all choices have been gotten.
   generateGameBoard();
 }
 btnSelect.forEach((btn, index) => {
   btn.addEventListener("click", () => handleGameOptionChoice(index));
 });
-// game screen functions
+// handles the creation of the board
 function generateGameBoard() {
   gameState.board.forEach(() => {
     const div = document.createElement("div");
     div.className += "game-cell";
     div.dataset.mark = "";
-    // later  we will check if the board has been filled.
     gameBoard.appendChild(div);
     document.querySelectorAll(".game-cell").forEach((cell, index) => {
       cell.addEventListener("click", () => handleCellClick(index));
     });
   });
-  /* if(gameState.player2 && gameState.mark.p2 === "O"){
-    gameState.playerTurn = 'O';
-  } */
+  // if the bot is meant to go first it clicks as soon as all cells have been created.
   if (gameState.cpu && gameState.mark.cpu === "X") {
     handleBotClick();
   }
   updateTally();
 }
 
-// handle cell click
+// runs when the user clicks a cell.
 let cpuTurn = false;
 function handleCellClick(index) {
   const gameCells = document.querySelectorAll(".game-cell");
-
+  // user cannot click a cell that is occupied or  when cpu is meant to click.
   if (gameCells[index].dataset.mark !== "" || cpuTurn) {
     return;
   }
@@ -123,10 +125,11 @@ function handleCellClick(index) {
     cpuTurn = true;
     setTimeout(() => {
       handleBotClick();
-      cpuTurn =false;
+      cpuTurn = false;
     }, 1500);
   }
 }
+// handles the ui update for the game.
 function updateGame(index, mark) {
   const gameCells = document.querySelectorAll(".game-cell");
   gameState.board[index] = mark;
@@ -154,6 +157,7 @@ function updateGame(index, mark) {
               <span class="${resultStyles[winnerMark].class}">takes the round</span>
             </p>
             `;
+    // end the game to stop bot from clicking.
     gameState.gameEnded = true;
     setTimeout(() => {
       resultModal.style.display = "grid";
@@ -178,6 +182,7 @@ function updateGame(index, mark) {
   }
   gameTurn.textContent = gameState.playerTurn;
 }
+// updates the ui with the latest tally scores.
 function updateTally() {
   tiesScore.innerHTML = gameState.tally.tie;
 
@@ -207,17 +212,20 @@ function updateTally() {
   p2Score.innerHTML = gameState.tally.p1;
   p2Name.innerHTML = gameState.cpu ? "O (YOU)" : "O (P1)";
 }
+// handles the cpus clicks.
 function handleBotClick() {
-  if(gameState.gameEnded) return;
+  if (gameState.gameEnded) return;
+  // dont have time to implement minmax so the cpu just clicks a random empty space.
   const emptySpace = findEmptySpace();
   updateGame(emptySpace, gameState.mark.cpu);
 }
+// used to find an empty space for the cpu.
 function findEmptySpace() {
   const space = gameState.board.filter((value) => typeof value === "number");
   return space[Math.floor(Math.random() * space.length)];
 }
+// used to search for a winner while playing returns an array of indexes or an empty array.
 function checkWinner() {
-  // returns either X or  O or ''
   const gameCells = document.querySelectorAll(".game-cell");
   let winner = [];
   const board = gameState.board;
@@ -230,7 +238,6 @@ function checkWinner() {
   if (winner.length) {
     const winnerIndex = winner[0];
     const winnerMark = gameState.board[winnerIndex];
-    /* #1F3641' */
     winner.forEach((item) => {
       gameCells[item].style.backgroundColor =
         gameState.board[winnerIndex] === "X" ? "#31C3BD" : "#F2B137";
@@ -239,9 +246,11 @@ function checkWinner() {
   }
   return winner;
 }
+// checks for a tie.
 function checkTie() {
   return gameState.board.every((value) => typeof value !== "number");
 }
+// gives a fresh game without recreating the board.
 function restartGame() {
   const gameCells = document.querySelectorAll(".game-cell");
   gameState.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -250,11 +259,13 @@ function restartGame() {
     gameCells[item].dataset.mark = "";
     gameCells[item].style.backgroundColor = "#1F3641";
   });
+  // a hack for when the cpu is first so on restart you always see its first move.
   if (gameState.cpu && gameState.mark.cpu === "X") {
     handleBotClick();
   }
 }
 gameRestart.addEventListener("click", restartGame);
+// handles the button clicks under the result modal.
 function handleResultOption(index) {
   if (btnResult[index].dataset.result === "Quit") {
     clearGame();
@@ -265,8 +276,8 @@ function handleResultOption(index) {
     return;
   }
   gameState.gameEnded = false;
-  if(gameState.playerTurn === 'O'){
-     gameState.playerTurn = "X";
+  if (gameState.playerTurn === "O") {
+    gameState.playerTurn = "X";
   }
   restartGame();
   resultModal.style.display = "none";
@@ -274,8 +285,9 @@ function handleResultOption(index) {
 btnResult.forEach((btn, index) =>
   btn.addEventListener("click", () => handleResultOption(index))
 );
-function clearGame() {
-  gameState = JSON.parse(JSON.stringify(initialGameState));
-  gameBoard.innerHTML = '';
 
+function clearGame() {
+  // a hack for deep copying an object.
+  gameState = JSON.parse(JSON.stringify(initialGameState));
+  gameBoard.innerHTML = "";
 }
